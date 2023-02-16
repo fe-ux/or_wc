14d30f046b95a8aad290eeda2771b111776adcaf
ArseniyAleynik <arseniyaleynik@yandex.ru>
2023-02-16 18:09:58 +0300

    modified:   main.py

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import numpy as np
 import os
+import pathlib
+
 import tensorflow as tf
 
 from object_detection.utils import ops as utils_ops
@@ -8,15 +10,20 @@ from object_detection.utils import visualization_utils as vis_util
 
 import cv2
 
+utils_ops.tf = tf.compat.v1
+
+tf.gfile = tf.io.gfile
 
 
 PATH_TO_LABELS = '/home/arseniy/Downloads/test/TensorFlow/models/research/object_detection/data/mscoco_label_map.pbtxt'
 category_index = label_map_util.create_category_index_from_labelmap(PATH_TO_LABELS, use_display_name=True)
 
-
+PATH_TO_TEST_IMAGES_DIR = pathlib.Path('/home/arseniy/Downloads/test/TensorFlow/models/research/object_detection/data/test_images')
+TEST_IMAGE_PATHS = sorted(list(PATH_TO_TEST_IMAGES_DIR.glob("*.jpg")))
+     
 os.chdir("/home/arseniy/main/or_wc/efficientdet_d2_coco17_tpu-32/saved_model/")
 detection_model = tf.saved_model.load("/home/arseniy/main/or_wc/efficientdet_d2_coco17_tpu-32/saved_model")
-
+     
 
 detection_model.signatures['serving_default'].inputs
 
@@ -26,7 +33,7 @@ detection_model.signatures['serving_default'].output_shapes
      
 
 def run_inference_for_single_image(model, image):
-    image = cv2.resize(image, [768, 768], interpolation = cv2.INTER_AREA)
+    image = cv2.resize(image, [1280, 1280], interpolation = cv2.INTER_AREA)
     image = np.asarray(image)
 
     input_tensor = tf.convert_to_tensor(image)
@@ -35,15 +42,14 @@ def run_inference_for_single_image(model, image):
 
     model_fn = model.signatures['serving_default']
     output_dict = model_fn(input_tensor)
-    
+
     num_detections = int(output_dict.pop('num_detections'))
     output_dict = {key:value[0, :num_detections].numpy() for key,value in output_dict.items()}
     output_dict['num_detections'] = num_detections
 
     output_dict['detection_classes'] = output_dict['detection_classes'].astype(np.int64)
-   
+
     if 'detection_masks' in output_dict:
-        
         detection_masks_reframed = utils_ops.reframe_box_masks_to_image_masks(
             output_dict['detection_masks'], 
             output_dict['detection_boxes'], 
@@ -78,14 +84,13 @@ while(True):
     ret, frame = cap.read()
     output_dict = run_inference_for_single_image(detection_model, frame)
     cv2.imshow('image window', cv2.resize(show_inference(frame, output_dict),[1600,720]))
-    for i in range(30):
+    for i in range(40):
         ret, frame = cap.read()
         cv2.imshow('image window', cv2.resize(show_inference(frame, output_dict),[1600,720]))
         if cv2.waitKey(1) & 0xFF == ord('q'):
-            cv2.destroyAllWindows()
             t=1
             break
-    if (cv2.waitKey(2) & 0xFF == ord('q')) or t:
+    if t:
         cv2.destroyAllWindows()
         break
     
